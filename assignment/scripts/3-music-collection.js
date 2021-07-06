c250fd9a3e2a68304716286dde1099dde286fc8e
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-07-06 16:14:20 -0500

    addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,13 @@
 console.log('***** Music Collection *****')
+
+const collection = [];
+
+function addToCollection(title, artist, yearPublished){
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  } //end object
+  collection.push(album);
+  return album;
+} //end function
