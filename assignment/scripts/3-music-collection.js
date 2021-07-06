8de165b3e226706d4523aa873ce17475bd628218
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-07-06 16:35:54 -0500

    addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,3 +11,18 @@ function addToCollection(title, artist, yearPublished){
   collection.push(album);
   return album;
 } //end function
+
+//Test for addToCollection function
+
+//Test for same artist
+console.log(addToCollection('Thriller', 'Michael Jackson', 1982));
+console.log(addToCollection('Bad', 'Michael Jackson', 1987));
+
+//Test for same year
+console.log(addToCollection('The Marshall Mathers LP', 'Eminem', 2000));
+console.log(addToCollection('Hybrid Theory', 'Linkin Park', 2000));
+
+//Test for not matching
+console.log(addToCollection('The Dark Side of the Moon', 'Pink Floyd', 1973));
+console.log(addToCollection('Nevermind', 'Nirvana', 1991));
+console.log(collection);
