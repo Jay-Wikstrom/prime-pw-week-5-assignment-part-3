61b68166f7053cbf122155711f68df8519121034
Jay Wikstrom <jaywikstrom@yahoo.com>
2021-07-10 15:16:16 -0500

    Added search function w/out track name

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -34,6 +34,29 @@ function findByArtist(artist){
   return array;
 } //end function
 
+function search(searchCriteria){
+  let array = [];
+  let i = 0;
+
+  for (i of collection)
+  {
+    if (searchCriteria.artist === i.artist && searchCriteria.year === i.yearPublished){
+      array.push(i.artist, i.yearPublished)
+      return array;
+    } //end if
+  } //end for loop
+
+    if (Object.keys(searchCriteria).length === 0){
+      array = collection;
+      return array;
+    } //end if
+
+    if(searchCriteria.artist !== i.artist || searchCriteria.year !== i.yearPublished){
+      return array;
+    } //end if
+} //end function
+
+
 //Add 4 tracks for each album
 let thriller = [
   ['Wanna Be Startin Something', 362],
@@ -93,77 +116,24 @@ console.log('*** Test for not matching ***');
 console.log(addToCollection('The Dark Side of the Moon', 'Pink Floyd', 1973, theDarkSideOfTheMoon));
 console.log(addToCollection('Nevermind', 'Nirvana', 1991, nevermind));
 
-console.log('*** collection logged to console ***');
+console.log('*** Collection logged to console ***');
 console.log(collection);
 
 //Test for showCollection
+console.log('')
 console.log('*** Test for show collection function ***')
 showCollection(collection);
 
 //Test for findByArtist
+console.log('')
 console.log('** Test for find by artist function ***');
 console.log(findByArtist('Eminem'));
 console.log(findByArtist(''));
 console.log(findByArtist('Michael Jackson'));
 
-//Failed search function attempts
-/*
-
-function search(searchCriteria){
-  for (let key in collection){
-    let array = [];
-    //console.log(collection[key]);
-    if(searchCriteria === collection[key]){
-      array.push(collection[key]);
-      console.log(collection[key]);
-    }
-  } //end for in loop
-} //end function
-
-
-
-
-function search(searchCriteria){
-  let array = [];
-  for (let i = 0; i < collection.length; i++){
-    if (collection[i].artist == searchCriteria){
-      array.push(searchCriteria);
-    }
-    return array;
-  }
-}
-
-
-function search(searchCriteria){
-  //collection.forEach(item => console.log(item))
-  array = [];
-  Object.keys(collection).forEach(key => {
-    if (key, collection[key] === searchCriteria){
-      array.push(searchCriteria);
-    }
-  })
-}
-
-
-
-/*let array = [];
-for (let index in collection){
-  if(collection.hasOwnProperty(index)){
-    array.push[searchCriteria];
-  }
-  return array;
-}
-
-//Test search function
-let searchResult = search({
-  title: 'Bad',
-  artist: 'Michael Jackson',
-  yearPublished: 1987
-})
-console.log(searchResult);
-//console.log(search('Michael Jackson', 1987));
-//console.log(search());
-//console.log(search('Ray Charles', 1957));
-
-
-*/
+//Test for search function
+console.log('')
+console.log('*** Test for search function ***')
+console.log(search({}));
+console.log(search({artist: 'Michael Jackson', year: 1987}));
+console.log(search({artist: 'Ray Charles', year: 1957}));
